93153c13029c90914b31703bb4e9b2da7f561afe
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-10-25 15:17:27 +0530

    Added ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+  constructor(public name: string, public amount: string) {}
+}
+
+
+// export class Ingredient {
+//   public name: string;
+//   public amount: string;
+
+//   constructor(name: string, amount: string) {
+//     this.name = name;
+//     this.amount = amount;
+//   }
+// }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('warm water', '355 ml'),
+    new Ingredient('active dry yeast', '2 1/4 teaspoons'),
+    new Ingredient('olive oil', '50 ml'),
+    new Ingredient('salt', '2 teaspoons'),
+    new Ingredient('sugar', '1 teaspoons'),
+    new Ingredient('Tomato sauce ', '1 package'),
+    new Ingredient('Firm mozzarella cheese ', '1 package'),
+    new Ingredient('Fontina cheese ', '1 package'),
+    new Ingredient('Feta cheese ', '1 package'),
+    new Ingredient('Mushrooms ', '200 g'),
+    new Ingredient('Bell peppers ', '200 g'),
+    new Ingredient('Pepperoni ', '150 g'),
+  ];
 
   constructor() { }
 
