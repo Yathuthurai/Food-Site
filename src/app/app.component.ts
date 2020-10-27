7a0937784322c8d7cd99bf9ad3a0b6718936d43e
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-10-27 21:06:46 +0530

    Navigation added to recipe and shopping list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FoodApp';
+  loadedFeature = 'recipe';
+
+  // tslint:disable-next-line:typedef
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
