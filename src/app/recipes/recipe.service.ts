c9935f9ae522194f7fa787fcae3f1928b638dd22
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-10-30 11:05:02 +0530

    Adding ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -8,9 +9,49 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Pasta', 'Italian testing pasta', 'https://www.eatwell101.com/wp-content/uploads/2017/11/sausage-and-pasta-recipes-for-dinner.jpg'),
-    new Recipe('Pizza', 'Italian cheese pizza', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190226-buffalo-chicken-pizza-370-1552084943.jpg'),
-    new Recipe('Burger', 'Double cheese chicken burger', 'https://www.saputo.ca/en/recipes/url(/-/media/ecosystem/divisions/canada-dairy/sites/saputo-ca/saputo-ca-images/recipes/hero/_mobile_images/hero_beef_and_asiago_burger_mobile.ashx?revision=8029944e-f76d-4b80-b0b9-c2b02afe5b8d)')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!😉',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/1200px-Wiener-Schnitzel02.jpg',
+      [
+        new Ingredient('Meat(g)', 350),
+        new Ingredient('French Fries(6)', 100)
+      ]),
+    new Recipe(
+      'Pizza',
+      'Italian cheese pizza',
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/190226-buffalo-chicken-pizza-370-1552084943.jpg',
+      [
+        new Ingredient('Flour(g)', 1000),
+        new Ingredient('Meat(g)', 250),
+        new Ingredient('Cheese(package)', 1),
+        new Ingredient('Pepperoni(package)', 1),
+        new Ingredient('Tomato Sauce(package)', 1),
+        new Ingredient('Red Pepper(g)', 200),
+        new Ingredient('Yellow Pepper(g)', 200),
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?😋',
+      'https://www.foodrepublic.com/wp-content/uploads/2012/03/033_FR11785.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat(g)', 250),
+        new Ingredient('Cheese(package)', 1),
+      ]),
+    new Recipe(
+      'Korean Sea food fried rice',
+      'Taste the real fried rice😋',
+      'https://img.taste.com.au/_Cn_i0k4/taste/2016/11/korean-fried-rice-110237-1.jpeg',
+      [
+        new Ingredient('Rice(g)', 500),
+        new Ingredient('Crab(g)', 250),
+        new Ingredient('Shrimp(g)', 250),
+        new Ingredient('Egg', 3),
+        new Ingredient('Soy Sauce(ml)', 100),
+        new Ingredient('Mint Leaves(g)', 100),
+        new Ingredient('Oil(ml)', 50),
+      ])
   ];
 
   constructor() { }
