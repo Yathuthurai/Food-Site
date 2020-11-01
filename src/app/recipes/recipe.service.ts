5d5fbf9f4061d2d6126431e7174d4330d6c89dbb
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-11-01 12:24:59 +0530

    Configured route parameters

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -61,6 +61,11 @@ export class RecipeService {
   }
 
   // tslint:disable-next-line:typedef
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  // tslint:disable-next-line:typedef
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
