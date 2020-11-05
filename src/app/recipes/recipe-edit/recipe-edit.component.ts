1060d13b7adcb6dcacdbaf93a1fe6491da54afbb
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-11-05 20:42:11 +0530

    Added react native approach

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,8 +11,9 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode: boolean;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   // tslint:disable-next-line:typedef
   ngOnInit() {
@@ -19,8 +22,31 @@ export class RecipeEditComponent implements OnInit {
         (params: Params) => {
           this.id = +params.id;
           this.editMode = params.id != null;
+          // this.initForm();
         }
       );
   }
-
 }
+
+// // tslint:disable-next-line:typedef
+// private initForm() {
+//   let recipeName: '';
+//   let recipeImagePath: '';
+//   let recipeDescription: '';
+//   if (this.editMode) {
+//     const recipe = this.recipeService.getRecipe(this.id);
+//     recipeName = recipe.name;
+//     // tslint:disable-next-line:no-unused-expression
+//     recipeImagePath: recipe.imagePath;
+//     // tslint:disable-next-line:no-unused-expression
+//     recipeDescription: recipe.description;
+//   }
+//   this.recipeForm = new FormGroup({
+//     // tslint:disable-next-line:object-literal-key-quotes
+//     'name': new FormControl(recipeName),
+//     // tslint:disable-next-line:object-literal-key-quotes
+//     'imagePath': new FormControl(recipeImagePath),
+//     // tslint:disable-next-line:object-literal-key-quotes
+//     'imageDescription': new FormControl(recipeDescription),
+//   });
+// }
