2bf78c474c4bf2d8d0a43001a5379c2cfcc36b5e
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-11-09 22:04:02 +0530

    Added storing data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+
+  // tslint:disable-next-line:typedef
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://foodsiteangular.firebaseio.com/recipes.json', recipes).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
