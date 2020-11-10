544461a4a169fee5516f791c9dfb0338d0d1ada6
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-11-10 12:31:50 +0530

    Prepared backend for authentication

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Interface } from 'readline';
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  // tslint:disable-next-line:typedef
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD_RbkFZ2LB5mzGu1OUDZwLKQtcB0sF_vM',
+      {
+        // tslint:disable-next-line:object-literal-shorthand
+        email: email,
+        // tslint:disable-next-line:object-literal-shorthand
+        password: password,
+        returnSecureToken: true
+      }
+      );
+  }
+}
