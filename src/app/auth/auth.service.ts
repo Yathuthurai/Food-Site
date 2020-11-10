50e4990a28985d20ed0af2d101378f878fb5010a
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-11-10 15:57:45 +0530

    Handled login error

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -33,18 +33,7 @@ export class AuthService {
         returnSecureToken: true
       }
       )
-      .pipe(catchError(errorRes => {
-        // tslint:disable-next-line:prefer-const
-        let errorMessage = 'An error occurred!';
-        if (!errorRes.error || !errorRes.error.error) {
-          return throwError(errorMessage);
-        }
-        switch (errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'This email already exists';
-        }
-        return throwError(errorMessage);
-      }));
+      .pipe(catchError(this.handleError));
   }
 
   // tslint:disable-next-line:typedef
@@ -56,6 +45,27 @@ export class AuthService {
       // tslint:disable-next-line:object-literal-shorthand
       password: password,
       returnSecureToken: true
-    });
+    }).pipe(catchError(this.handleError));
+  }
+
+  // tslint:disable-next-line:typedef
+  private handleError(errorRes: HttpErrorResponse) {
+    // tslint:disable-next-line:prefer-const
+    let errorMessage = 'An error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exists.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct.';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
