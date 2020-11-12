3e4165d85b450e6ebd4f0eec4723b55a082b5310
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-11-12 20:38:24 +0530

    Added AutoLogin

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -62,6 +62,32 @@ export class AuthService {
         const user = new User(email, userId, token, expirationDate);
           // tslint:disable-next-line:align
           this.user.next(user);
+          // tslint:disable-next-line:align
+          localStorage.setItem('userData', JSON.stringify(user));
+  }
+
+  // tslint:disable-next-line:typedef
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
   }
 
   // tslint:disable-next-line:typedef
