abbc7de88630b41a13e2eb36490b815fa260206c
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-11-18 10:09:17 +0530

    Added Core module

---
+++ src/app/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeService } from './recipes/recipe.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
+  ]
+})
+export class CoreModule { }
